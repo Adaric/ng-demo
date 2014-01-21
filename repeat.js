7eed78a33ea960428470cf2b09938ce20d1e3aa3
Adaric <nigermiles4@gmail.com>
2014-01-21 19:14:41 +0530

    ng-repeat

---
+++ repeat.js
@@ -0,0 +1,9 @@
+function question($scope)
+{$scope.questions = [
+	{q:'Capital of India',l:'easy'},
+	{q:'location of hanging gardens',l:'medium'},
+	{q:'longest ruling dictator',l:'medium'},
+	{q:'first computer programmer',l:'hard'},
+	{q:'50th president of us',l:'hard'},
+	{q:'biggest moon of jupiter',l:'medium'}
+	];}
