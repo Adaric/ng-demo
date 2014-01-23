3460e43f12d2b41d33cf3a81fa367eca0a03647e
Adaric <nigermiles4@gmail.com>
2014-01-23 13:45:34 +0530

    complete  version

--- repeat.js
+++ repeat.js
@@ -1,14 +1,14 @@
 function question($scope)
-{$scope.questions = [
-	{q:'Capital of India',l:'easy'},
+{$scope.questions = 
+	[[{q:'Capital of India',l:'easy'},
 	{q:'location of hanging gardens',l:'medium'},
-	{q:'longest ruling dictator',l:'medium'},
-	{q:'first computer programmer',l:'hard'},
+	{q:'longest ruling dictator',l:'medium'}],
+	[{q:'first computer programmer',l:'hard'},
+	{q:'50th president of us',l:'easy'},
+	{q:'50th president of us',l:'hard'}],
+	[{q:'50th president of us',l:'medium'},
 	{q:'50th president of us',l:'hard'},
-	{q:'50th president of us',l:'hard'},
-	{q:'50th president of us',l:'hard'},
-	{q:'50th president of us',l:'hard'},
-	{q:'50th president of us',l:'hard'},
-	{q:'50th president of us',l:'hard'},
-	{q:'biggest moon of jupiter',l:'medium'}
-	];}
+	{q:'50th president of us',l:'easy'}]];
+	
+//	$scope.mystyle="{display:table-cell}";
+	}
