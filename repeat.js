9b61761ab47fd8609bd2dfdde2c18ed5c098318d
Adaric <nigermiles4@gmail.com>
2014-01-23 14:45:45 +0530

    new1

--- repeat.js
+++ repeat.js
@@ -1,14 +1,38 @@
 function question($scope)
 {$scope.questions = 
-	[[{q:'Capital of India',l:'easy'},
+	[
+	[{q:'Capital of India',l:'easy'},
 	{q:'location of hanging gardens',l:'medium'},
-	{q:'longest ruling dictator',l:'medium'}],
+	{q:'longest ruling dictator',l:'medium'},
+	{q:'50th president of us',l:'hard'},
+	{q:'location of hanging gardens',l:'medium'}
+	],
 	[{q:'first computer programmer',l:'hard'},
 	{q:'50th president of us',l:'easy'},
-	{q:'50th president of us',l:'hard'}],
+	{q:'50th president of us',l:'hard'},
+	{q:'50th president of us',l:'easy'},
+	{q:'50th president of us',l:'hard'}
+	],
 	[{q:'50th president of us',l:'medium'},
 	{q:'50th president of us',l:'hard'},
-	{q:'50th president of us',l:'easy'}]];
+	{q:'50th president of us',l:'easy'},
+	{q:'50th president of us',l:'medium'},
+	{q:'50th president of us',l:'easy'}
+	],
+	[{q:'Capital of India',l:'easy'},
+	{q:'location of hanging gardens',l:'medium'},
+	{q:'longest ruling dictator',l:'medium'},
+	{q:'50th president of us',l:'hard'},
+	{q:'location of hanging gardens',l:'medium'}
+	],
+	[{q:'first computer programmer',l:'hard'},
+	{q:'50th president of us',l:'easy'},
+	{q:'50th president of us',l:'hard'},
+	{q:'50th president of us',l:'easy'},
+	{q:'50th president of us',l:'hard'}
+	]
+	
+	];
 	
 //	$scope.mystyle="{display:table-cell}";
 	}
